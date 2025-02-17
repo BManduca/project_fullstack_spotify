bb067f1b24ca0bb77303893c0b9bc6ea5f89d83d
Brunno Manduca <brunnomanducarfe@gmail.com>
2025-02-16 21:05:36 -0300

    feat: ajustes para realização do deploy

--- Projeto/back-end/api/server.js
+++ Projeto/back-end/api/server.js
@@ -1,11 +1,15 @@
 // API -> Application Programming Interface
 // sistema utilizado para comunicação de duas ou mais entidades
 
-import express from 'express';
-import cors from 'cors';
-import { db } from './connect.js';
+import express from "express";
+import cors from "cors";
+import { db } from "./connect.js";
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import path from "path";
 
-// trazendo todo o 'poder' que o express tem
+const __dirname = path.resolve()
+
+// trazendo todo o "poder" que o express tem
 // armazenando dentro da variável app
 // e assim iremos gerenciar toda a nossa api
 const app = express();
@@ -13,16 +17,22 @@ const PORT = 3001;
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Home...')
+app.get("/api/", (req, res) => {
+    res.send("Trabalhando com os endpoints '/artists' e '/songs'")
 })
 
-app.get('/artists', async (req, res) => {
+app.get("/api/artists", async (req, res) => {
     res.send(await db.collection("artists").find({}).toArray())
 })
 
-app.get('/songs', async (req, res) => {
+app.get("/api/songs", async (req, res) => {
     res.send(await db.collection("songs").find({}).toArray())
+})
+
+app.use(express.static(path.join(__dirname, '../front-end/dist')))
+
+app.get("*", async (req, res) => {
+    res.sendFile(path.join(__dirname, '../front-end/dist/index.html'),)
 })
 
 app.listen(PORT, () => {
