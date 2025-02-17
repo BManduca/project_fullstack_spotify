769fe9363d68479fc894354c0ddfaac7f71d9b58
Brunno Manduca <brunnomanducarfe@gmail.com>
2025-02-17 17:21:14 -0300

    ajustes finais para efetuar deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import "dotenv/config"
 
-// const { NODE_ENV } = process.env;
-const URL = "http://localhost:3001/api";
+const { NODE_ENV } = process.env
+
+const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const URL = "http://localhost:3001/api";
 
 const responseArtists = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
